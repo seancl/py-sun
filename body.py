e83c918b4809076fee287563ac064ff531c770a9
Sean Lindsay <seancl4@gmail.com>
2017-09-05 03:44:30 -0700

    Refactor vectors and positions into tuples
    Also start using comprehensions to simplify logic now that I understand how those work

--- body.py
+++ body.py
@@ -11,48 +11,35 @@ class Body:
 		self.vel = velocity # in m/s
 
 	def updateVelocity(self, bodies, elapsedTime):
-		netForce = {'x': 0, 'y': 0}
-		for body in bodies:
-			accel = self.gravityVector(body)
-			netForce['x'] += accel['x']
-			netForce['y'] += accel['y']
-		self.vel['x'] += netForce['x'] * elapsedTime
-		self.vel['y'] += netForce['y'] * elapsedTime
+		vectors = list(self.gravityVector(body) for body in bodies)
+		netAccel = tuple(sum(v) for v in zip(*vectors))
+		self.vel = tuple(v + a*elapsedTime for v,a in zip(self.vel, netAccel))
 
 	def updatePosition(self, elapsedTime):
-		self.pos['x'] += self.vel['x'] * elapsedTime
-		self.pos['y'] += self.vel['y'] * elapsedTime
+		self.pos = tuple(p + v*elapsedTime for p,v in zip(self.pos, self.vel))
 
 	def distanceTo(self, otherBody):
-		return (
-			(self.pos['x'] - otherBody.pos['x']) ** 2
-			+ (self.pos['y'] - otherBody.pos['y']) ** 2
-		) ** 0.5
+		return sum((s - ob)**2 for s,ob in zip(self.pos, otherBody.pos)) ** 0.5
 
 	def gravityVector(self, body):
 		dist = self.distanceTo(body)
 
 		if dist == 0:
-			return {'x': 0, 'y': 0}
+			return (0, 0)
 
 		accel = G * body.mass / dist**2
-		return {
-			'x': -accel * (self.pos['x'] - body.pos['x']) / dist,
-			'y': -accel * (self.pos['y'] - body.pos['y']) / dist
-		}
+		return tuple(-accel * (s - b) / dist for s,b in zip(self.pos, body.pos))
 
 	def draw(self, screen, cameraPos):
 		height,width = screen.getmaxyx()
-		centerX = round(width / 2)
-		centerY = round(height / 2)
-		x = centerX + round((self.pos['x'] - cameraPos['x']) * self.scale)
-		y = centerY - round((self.pos['y'] - cameraPos['y']) * self.scale)
+		middlePoint = (round(width/2), round(height/2))
+		x,y = tuple(m + round((s - c) * self.scale) for m,s,c in zip(middlePoint, self.pos, cameraPos))
 		if 0 <= x < width and 0 <= y < height:
-			screen.addstr(y, x, self.symbol)
+			screen.addstr(height - y, x, self.symbol)
 
 	def dump(self, screen, offset):
 		screen.addstr(offset + 0, 0, "mass: " + str(self.mass))
-		screen.addstr(offset + 1, 0, "x: " + str(round(self.pos['x'] / AU, 5)) + " AU")
-		screen.addstr(offset + 2, 0, "y: " + str(round(self.pos['y'] / AU, 5)) + " AU")
-		screen.addstr(offset + 3, 0, "delta x: " + str(self.vel['x']))
-		screen.addstr(offset + 4, 0, "delta y: " + str(self.vel['y']))
+		screen.addstr(offset + 1, 0, "x: " + str(round(self.pos[0] / AU, 5)) + " AU")
+		screen.addstr(offset + 2, 0, "y: " + str(round(self.pos[1] / AU, 5)) + " AU")
+		screen.addstr(offset + 3, 0, "delta x: " + str(self.vel[0]))
+		screen.addstr(offset + 4, 0, "delta y: " + str(self.vel[1]))
